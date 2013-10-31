e8b0f15564b11fd89813402b5b7a86a1bbfe604a
ljh198275823 <ljh198275823@163.com>
2013-10-31 22:58:53 +0800

    d

--- Source/Trunck/ReferenceDll/DbUpdate.sql
+++ Source/Trunck/ReferenceDll/DbUpdate.sql
@@ -1,1 +1,10 @@
-select 'hello world'
+--
+----add by bruce 2013-9-12
+--if not exists (SELECT * FROM dbo.syscolumns WHERE name ='Memo' AND id = OBJECT_ID(N'[dbo].[TAOperator]')) 
+--IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[TAParameter]') AND type in (N'U'))
+
+if not exists (SELECT * FROM dbo.syscolumns WHERE name ='Memo' AND id = OBJECT_ID(N'[dbo].[AttachmentHeader]')) 
+begin
+	alter table AttachmentHeader add Memo nvarchar(200)
+end 
+go
