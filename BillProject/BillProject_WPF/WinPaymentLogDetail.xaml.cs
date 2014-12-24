be61dbc5ee489ca657b1a5c727a2840e3a6157a8
ljh198275823 <ljh198275823@163.com>
2014-12-24 15:35:05 +0800

    1.完善了一下WPF的记账小秘

--- BillProject/BillProject_WPF/WinPaymentLogDetail.xaml.cs
+++ BillProject/BillProject_WPF/WinPaymentLogDetail.xaml.cs
@@ -10,17 +10,31 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using LJH.BillProject .Model ;
+using LJH.BillProject .BLL ;
 
 namespace BillProject_WPF
 {
     /// <summary>
     /// Window1.xaml 的交互逻辑
     /// </summary>
-    public partial class Window1 : Window
+    public partial class WinPaymentLogDetail : Window
     {
-        public Window1()
+        public WinPaymentLogDetail()
         {
             InitializeComponent();
+        }
+
+        #region 公共属性
+        public List<PaymentLog> PaymentLogs { get; set; }
+        #endregion
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            if (PaymentLogs != null)
+            {
+            }
         }
     }
 }
