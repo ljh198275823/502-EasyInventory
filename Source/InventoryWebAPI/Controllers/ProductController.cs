f5dd8fa22014de86037528ac69fe45ba6d689778
ljh198275823 <ljh198275823@163.com>
2015-04-01 14:30:39 +0800

    1.增加一个WEBAPI

---
+++ Source/InventoryWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Configuration;
+using LJH.Inventory.BLL;
+using LJH.Inventory.BusinessModel;
+
+namespace LJH.Inventory.WebAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        public string[] GetAll()
+        {
+            List<Product> ps = new ProductBLL(WebConfigurationManager .ConnectionStrings["Inventory"].ConnectionString ).GetItems(null).QueryObjects;
+            if (ps != null && ps.Count > 0)
+            {
+                return ps.Select(it => it.Name).ToArray();
+            }
+            return new string[] { "not found" };
+        }
+    }
+}
