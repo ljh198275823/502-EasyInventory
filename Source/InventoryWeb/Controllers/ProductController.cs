855cb3a4d4954dd140a4eb72171cc8d1860333ce
ljh198275823 <ljh198275823@163.com>
2015-04-30 16:45:23 +0800

    1

--- Source/InventoryWeb/Controllers/ProductController.cs
+++ Source/InventoryWeb/Controllers/ProductController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LJH.Inventory.BLL;
+using LJH.Inventory.BusinessModel;
+using LJH.GeneralLibrary.Core.DAL;
 
 namespace InventoryWeb.Controllers
 {
@@ -11,10 +14,12 @@ namespace InventoryWeb.Controllers
         //
         // GET: /Product/
 
-        public string Index()
+        public ActionResult Index()
         {
-            return "hello world";
+            string conStr = string.Format("SQLITE:Data Source={0}", System.Web.Configuration.WebConfigurationManager.ConnectionStrings["Inventory"]);
+            List<Product> ps = new ProductBLL(conStr).GetItems(null).QueryObjects;
+            ViewBag.Products = ps;
+            return View();
         }
-
     }
 }
---
+++ Source/InventoryWeb/Controllers/ProductTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryWeb.Controllers.MVC
+{
+    public class ProductTest : Controller
+    {
+        public ActionResult  Index()
+        {
+            return View();
+        }
+
+    }
+}
