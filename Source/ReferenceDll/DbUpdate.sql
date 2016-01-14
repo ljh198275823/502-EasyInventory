3ee92d9bb2d2149ddfc23ec2ffddfe6b9dcc29d0
ljh198275823 <ljh198275823@163.com>
2016-01-14 16:45:56 +0800

    1

--- Source/ReferenceDll/DbUpdate.sql
+++ Source/ReferenceDll/DbUpdate.sql
@@ -17,3 +17,10 @@ BEGIN
 	exec ('alter table Customer add DefaultLinker uniqueidentifier')
 end
 go
+
+--2015-01-14
+if not exists (SELECT * FROM dbo.syscolumns WHERE name ='OriginalThick' AND id = OBJECT_ID(N'[dbo].[ProductInventoryItem]'))
+BEGIN
+	exec ('alter table ProductInventoryItem add OriginalThick decimal(18,4)')
+end
+go
