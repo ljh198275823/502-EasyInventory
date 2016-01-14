c1671c6e15fad6dac946df03184c52b366b51ec7
ljh198275823 <ljh198275823@163.com>
2016-01-14 16:49:09 +0800

    1

--- Source/ReferenceDll/DbUpdate.sql
+++ Source/ReferenceDll/DbUpdate.sql
@@ -1,26 +1,14 @@
----修改于2015-12-13 
-if exists (SELECT * FROM dbo.syscolumns WHERE name ='TotalWeight' AND id = OBJECT_ID(N'[dbo].[StackOutItem]'))
+
+--2015-11-26 送货单项增加TOTALWEIGHT字段
+if not exists (SELECT * FROM dbo.syscolumns WHERE name ='TotalWeight' AND id = OBJECT_ID(N'[dbo].[StackOutItem]'))
 BEGIN
-    exec ('update stackoutItem set length=weight ')
-	exec ('update stackoutItem set weight=totalweight ')
-	exec ('alter table StackOutItem drop column TotalWeight')
+	exec ('alter table StackOutItem add TotalWeight decimal(18,4) null')
 end
 go
 
-
-update productinventoryitem set model='原材料' where model='卷'
-go
-
----修改于2015-12-21
-if not exists (SELECT * FROM dbo.syscolumns WHERE name ='DefaultLinker' AND id = OBJECT_ID(N'[dbo].[Customer]'))
-BEGIN
-	exec ('alter table Customer add DefaultLinker uniqueidentifier')
-end
-go
-
---2015-01-14
+--2015-11-27 
 if not exists (SELECT * FROM dbo.syscolumns WHERE name ='OriginalThick' AND id = OBJECT_ID(N'[dbo].[ProductInventoryItem]'))
 BEGIN
-	exec ('alter table ProductInventoryItem add OriginalThick decimal(18,4)')
+	exec ('alter table ProductInventoryItem add OriginalThick decimal(18,4) null')
 end
 go
